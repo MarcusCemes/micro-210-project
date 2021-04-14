ee282c44febf7a74b334dbc95c0f94dc5b8e5d7f
Marcus Cemes <20288819+MarcusCemes@users.noreply.github.com>
2021-04-14 21:52:19 +0200

    feat: Basic LCD driver (#1)
    * feat: create LCD driver
    
    * feat: basic LCD driver, macros
    
    - Added a basic LCD driver to initialise and interface with the peripheral
    - Added some common macros to manipulate registers and make return breaks easier

--- Source/macros.asm
+++ Source/macros.asm
@@ -1,8 +1,79 @@
+; file: macros.asm      target: ATmega128L-4MHz-STK300
+; General use macros to avoid duplication.
+
+; === Register manipulation ===
+
 ; Load Stack Pointer
-; @0: address constant
-.macro  LDSP
-  ldi   w, low(@0)
-  out   spl,r16
-  ldi   w,high(@0)
-  out   sph,r16
+; @0: Data address
+.macro LDSP
+    ldi     w, low(@0)
+    out     spl,r16
+    ldi     w, high(@0)
+    out     sph,r16
+.endmacro
+
+; Out Immediate
+.macro OUTI
+    ldi     w, @1
+    out     @0, w
+.endmacro
+
+; Out Extended Immediate
+.macro OUTEI
+    ldi     w, @1
+    sts     @0, w
+.endmacro
+
+; Set bits in an I/O register
+; @0: I/O register
+; @1: Bit index
+.macro IOS
+    in      w, @0
+    sbr     w, @1
+    out     @0, w
+.endmacro
+
+; Clear bits in an I/O register
+; @0: I/O register
+; @1: Bit index
+.macro IOC
+    in      w, @0
+    sbc     w, @1
+    out     @0, w
+.endmacro
+
+
+; == Pointers ==
+
+; Load an immediate value into the Z register
+.macro LDIZ
+    ldi     zl, low(@0)
+    ldi     zh, high(@0)
+.endmacro
+
+; Multiply the z register by two
+.macro MUL2Z
+    lsl     zl    ; Shift all bits to the left
+    rol     zh    ; Restore the MSB from the SREG_C (Carry flag)
+.endmacro
+
+
+; === Utility ===
+
+; Call With
+; Load an immediate value into the w register and rcall a function.
+; @0: Function (r-called)
+; @1: Constant
+.macro CW
+    ldi     w, @1
+    rcall   @0
+.endmacro
+
+; Call With Extended
+; Load an immediate value into the w register and call a function.
+; @0: Function (called)
+; @1: Constant
+.macro CWE
+    ldi     w, @1
+    call    @0
 .endmacro
